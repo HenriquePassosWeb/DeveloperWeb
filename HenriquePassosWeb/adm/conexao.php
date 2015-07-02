bc4be7e6b93c9790aec625c786ac1c6a5553aca9
HenriquePassosWeb <henriquepassosweb@outlook.com>
2015-07-02 16:58:56 -0300

    Adiciona conexão com banco de dados

---
+++ HenriquePassosWeb/adm/conexao.php
@@ -0,0 +1,7 @@
+ <?php 
+	 
+	$con = mysql_connect("mysql.hostinger.com.br", "u185870019_henri", "henri120") or die ("Erro FTP access!");
+	
+	$banco = mysql_select_db("u185870019_dev01") or die ("Error ao conectar com o banco de dados");
+
+?>
