73b2d48bdc1e5078b10f7ce43f1af9dce3559a38
HenriquePassosWeb <henriquepassosweb@outlook.com>
2015-07-02 16:57:33 -0300

    Cadastro de categorias

---
+++ HenriquePassosWeb/adm/categoria/cadastro_categoria.php
@@ -0,0 +1,26 @@
+<?PHP
+
+require_once('../conexao.php');
+
+$categoria = $_POST['txt_categoria'];
+
+
+	                // Cria uma query MySQL
+	               $sql = "insert into tb_categoria (
+							   descricaocategoria						 
+							   ) 
+							   
+							   values(
+									   '$categoria'
+									 )";
+	                // Executa a consulta
+	               $query = mysql_query($sql);
+				 
+	                if ($query == true) {
+	                    echo 'Categoria inserido com sucesso!';
+						//header("Location:form_prod.php"); 
+	    } else {
+	        echo 'Ocorreu algum erro com o upload, por favor tente novamente!';
+	    }
+	 
+?>
